eca09c8b2edd4717eb4275dde8d8ea8bcac50efd
Poindexter Frink <sliderule@gmail.com>
2017-10-25 18:28:22 -0400

    Changed scale to 10000

--- src/chaos.c
+++ src/chaos.c
@@ -3,14 +3,14 @@
 static chaos_state_t chaos_state = { 0.5, 3.75 };
 
 void chaos_set_val(int16_t val) {
-    chaos_state.value = (float)val / (INT16_MAX * 1.0);
+    chaos_state.value = (float)val / (10000.0);
 }
 
 int16_t chaos_get_val() {
     // Logistic map
     chaos_state.value =
         chaos_state.r * chaos_state.value * (1.0 - chaos_state.value);
-    return (int16_t)(chaos_state.value * INT16_MAX);
+    return (int16_t)(chaos_state.value * 10000);
 }
 
 void chaos_set_r(int16_t r) {
