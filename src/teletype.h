a05d23171b0e02efe8b7be8cabbc4c88bb0bb5eb
Poindexter Frink <sliderule@gmail.com>
2017-10-28 17:13:24 -0400

    Bumped version number for alpha 1 build

--- src/teletype.h
+++ src/teletype.h
@@ -8,7 +8,7 @@
 #include "command.h"
 #include "state.h"
 
-#define TELETYPE_VERSION "TELETYPE 2.1.0"
+#define TELETYPE_VERSION "TELETYPE 2.2.0-ALPHA.1"
 #define TELE_ERROR_MSG_LENGTH 16
 
 typedef enum {
