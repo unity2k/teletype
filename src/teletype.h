cad5c21bb370e23abfd89941c96ed6dcd4ab8d73
Poindexter Frink <sliderule@gmail.com>
2017-12-19 13:41:37 -0500

    Disabled Profiling

--- src/teletype.h
+++ src/teletype.h
@@ -9,7 +9,7 @@
 #include "state.h"
 
 #define TELE_ERROR_MSG_LENGTH 16
-#define TELETYPE_PROFILE // un-comment this line to enable profiling
+// #define TELETYPE_PROFILE // un-comment this line to enable profiling
 
 typedef enum {
     E_OK,
